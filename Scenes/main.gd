a39774c25249988881775661531d5eb17d3f9cf7
Yolo-Arts <Jackteong123@gmail.com>
2025-11-09 19:05:53 +0000

    fixed scoreboard bug

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -5,9 +5,16 @@ extends Node2D
 @onready var game_over = $UI/GameOver
 
 func _ready():
+	Globals.playerDied = false
 	Globals.camera = camera
 	game_over.visible = false
 	Globals.player_died.connect(show_game_over_screen)
+
+	var final_score = Globals.score
+	for key in final_score:
+		final_score[key] = 0
+
+
 
 func show_game_over_screen():
 	game_over.visible = true
