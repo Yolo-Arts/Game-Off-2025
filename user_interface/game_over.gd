9d798b5b0bdde4bff231a2f51c63c0362d3b617b
Yolo-Arts <125148189+Yolo-Arts@users.noreply.github.com>
2025-11-12 16:07:27 +0000

    animating scoreboard

--- user_interface/game_over.gd
+++ user_interface/game_over.gd
@@ -2,6 +2,8 @@ extends CanvasLayer
 
 func _ready():
 	Globals.player_died.connect(display_scores)
+	
+	animate_stats()
 
 func _on_menu_pressed():
 	print("Menu Pressed")
@@ -17,6 +19,12 @@ func _on_restart_pressed():
 @onready var bosses_shipwrecked_score = %bosses_shipwreckedScore
 @onready var total_score_label = %total_score
 
+
+
+@onready var score_categories: VBoxContainer = $Control/Scoreboard/VBoxContainer/Scoreboard/MarginContainer2/CategoriesAndScoresDivider/ScoreCategories
+
+
+
 func display_scores():
 	var final_score = Globals.score
 	seconds_score.text = str(final_score["PER_SECOND_SURVIVED"])
@@ -29,3 +37,10 @@ func display_scores():
 	for key in final_score:
 		total_score += final_score[key]
 	total_score_label.text = str(total_score)
+
+func animate_stats() -> void:
+	var tween: Tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
+	
+	for child in score_categories.get_children():
+		tween.tween_property(child, "position:x", 0.0, 0.25).from(-get_viewport().size.x)
+		tween.tween_property(child, "modulate:a", 1.0, 0.25).from(0.0)
--- user_interface/healthbar.gd
+++ user_interface/healthbar.gd
@@ -3,6 +3,6 @@ extends CanvasLayer
 @onready var player: PackedScene = preload("res://Player/player.tscn")
 @onready var health_bar: ProgressBar = %HealthBar
 
-
-func _process(delta: float) -> void:
-	health_bar.value = player.health
+#
+#func _process(delta: float) -> void:
+	#health_bar.value = player.health
