63248629b227a34607d196891e857d1f5d4ed748
Yolo-Arts <Jackteong123@gmail.com>
2025-11-24 16:44:23 +0000

    added day night cycle for the giggles.

--- Player/player.gd
+++ Player/player.gd
@@ -15,7 +15,7 @@ var cannonball_scale = 1.0
 @export var turn_acceleration: float = 0.4
 @export var acceleration: float = 5.0
 @export var deceleration: float = 0.15
-@export var bounce_dampening: float = 1
+@export var bounce_dampening: float = 0.7
 @onready var drift:Timer = $drift
 @export var momentum_factor: float = 3.0  # Higher values = more momentum (more drift)
 @export var boost_decay: float = 14
