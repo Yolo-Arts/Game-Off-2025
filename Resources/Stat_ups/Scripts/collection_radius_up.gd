d96b313e666d7c00587e0cfed406bc71fe48369b
Yolo-Arts <Jackteong123@gmail.com>
2025-12-01 14:53:49 +0000

    buffed exp collection radius upgrade by 300%

--- Resources/Stat_ups/Scripts/collection_radius_up.gd
+++ Resources/Stat_ups/Scripts/collection_radius_up.gd
@@ -2,4 +2,4 @@ class_name Collection_radius_stat_up
 extends Statup
 
 func apply_upgrade(player: Player):
-	player.exp_collection_radius_shape.shape.radius += 12.0
+	player.exp_collection_radius_shape.shape.radius += 50.0
